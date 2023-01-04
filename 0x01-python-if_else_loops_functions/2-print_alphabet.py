61b00c45445aa582b27aecf18b44ea435bae45f2
humphreymbugua <humphreeydiaz@gmail.com>
2023-01-04 08:49:52 -0800

    a program that prints the ASCII alphabet, in lowercase

---
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+string = ""
+for i in "abcdefghijklmnopqrstuvwxyz":
+    string += i
+print("{0}".format(string), end="")
--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-string = ""
-for i in "abcdefghijklmnopqrstuvwxyz":
-    if (i == 'e') or (i == 'q'):
-        pass
-    else:
-        string += i
-print("{}".format(string), end="")
