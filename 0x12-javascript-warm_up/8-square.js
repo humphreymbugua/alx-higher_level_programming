8289efa4c8730d4a1fe8f586e0d16dd642ded61e
humphrey mbugua <humphreeydiaz@gmail.com>
2023-04-11 21:52:06 +0000

    JavaScript - Warm up.||

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 if (process.argv[2] === undefined || isNaN(process.argv[2])) {
   console.log('Missing size');
-} else }
+} else {
   const x = Number(process.argv[2]);
   let i = 0; 
   while (i < x) {
