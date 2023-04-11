4b6071e09b2fdf3348c40890ba47223302935ba2
humphrey mbugua <humphreeydiaz@gmail.com>
2023-04-11 23:59:07 +0000

    JavaScript - Warm up.||

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -4,9 +4,9 @@
 function factorial (n) {
   if ((isNaN(n)) || (n === 1)) {
     return 1;
-   } else {
+  } else {
      return n * factorial(n - 1);
-   }
+  }
 }
 
 console.log(factorial(parseInt(process.argv[2])));
