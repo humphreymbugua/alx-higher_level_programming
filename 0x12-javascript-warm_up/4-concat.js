63759369a916fdca653c4c14df918fcae82319a1
humphrey mbugua <humphreeydiaz@gmail.com>
2023-04-11 20:19:47 +0000

    JavaScript - Warm up.||

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-if (process.argv[2] !== undefined && process.argv[3] !== undefined) {
+if (process.argv[2] && process.argv[3]) {
   console.log(`${process.argv[2]} is ${process.argv[3]}`);
 } else {
-  console.log('Please provide at least two arguments.');
+  console.log(`${process.argv[2]} is ${process.argv[3]}`);
 }
